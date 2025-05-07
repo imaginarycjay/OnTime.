3b647322bece3e0122cf8d6423d2ca331d58dc24
imaginarycjay <imaginarycjay@gmail.com>
2025-05-07 13:04:25 +0800

    time is now running!

--- src/content.jsx
+++ src/content.jsx
@@ -4,7 +4,8 @@ import TaskManager from "./tasks.jsx";
 import Modal from "./modal.jsx";
 
 function MainContent() {
-  const [state, setState] = useState("25:00");
+  const [currentTime, setCurrentTime] = useState(25 * 60);
+  const [timeRunning, setTimeRunning] = useState(false);
   const [activeBtn, setActiveBtn] = useState("pomodoro");
   const [focusType, setFocusType] = useState("Time to Focus!");
   const [modalOpen, setModalOpen] = useState(false);
@@ -12,11 +13,50 @@ function MainContent() {
     const stored = localStorage.getItem("myTODOs");
     return stored ? JSON.parse(stored) : [];
   });
+
+  // handle time logic
+  useEffect(() => {
+    let realTime;
+
+    //time running logic
+    if (currentTime && timeRunning > 0) {
+      realTime = setInterval(() => {
+        setCurrentTime((prev) => prev - 1);
+      }, 1000);
+    }
+
+    //stops when the time is 0 with extra error handling
+    if (realTime === 0) {
+      setTimeRunning(false);
+      alert('Focus done, time for a short break!')
+    }
+
+    return () => {
+      clearInterval(realTime);
+    };
+  }, [currentTime, timeRunning]);
+
+  //format time
+  function formattedTime(time) {
+    const mins = Math.floor(time / 60);
+    const sec = time % 60;
+
+    return `${String(mins).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
+  }
+
+  //function to start time
+  function startTime() {
+    setTimeRunning((prev) => !prev);
+  }
+
   //function if the pomo, break, longbreak is clicked.
-  const handleClick = (type, time, focus, bg, bg2) => {
-    setState(time);
+  const handleClick = (type, time, timeRunning, focus, bg, bg2) => {
     setActiveBtn(type);
+    setCurrentTime(time);
+    setTimeRunning(timeRunning);
     setFocusType(focus);
+
+    //for styles
     document.documentElement.style.setProperty("--primary-bg-color", bg);
     document.documentElement.style.setProperty("--secondary-bg-color", bg2);
   };
@@ -56,7 +96,8 @@ function MainContent() {
             onClick={() =>
               handleClick(
                 "pomodoro",
-                "25:00",
+                5,
+                setTimeRunning(false),
                 "Time to Focus",
                 "#be3d2a",
                 "#d57d70"
@@ -74,7 +115,8 @@ function MainContent() {
             onClick={() =>
               handleClick(
                 "short",
-                "5:00",
+                5 * 60,
+                setTimeRunning(false),
                 "Time for Break",
                 "#169c3e",
                 "#4cb16a"
@@ -92,7 +134,8 @@ function MainContent() {
             onClick={() => {
               handleClick(
                 "long",
-                "15:00",
+                15 * 60,
+                setTimeRunning(false),
                 "Time for Long Break",
                 "#d97217",
                 "#e3934d"
@@ -110,9 +153,11 @@ function MainContent() {
         <div>
           <div className="main-card">
             <p className="prompt-title">{focusType}</p>
-            <p className="main-time">{state}</p>
+            <p className="main-time">{formattedTime(currentTime)}</p>
             <div className="button-container">
-              <button className="start-button">START</button>
+              <button onClick={startTime} className="start-button">
+                {!timeRunning ? "START" : "PAUSE"}
+              </button>
               <button onClick={toggleModal} className="add-task-button">
                 Add Task +
               </button>
