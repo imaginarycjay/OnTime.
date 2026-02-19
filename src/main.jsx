6a4bfc14731724e79b31b3764447948c4c9f9a73
imaginarycjay <imaginarycjay@gmail.com>
2026-02-20 01:06:59 +0800

    fix vercel build: remove invalid runtime, add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app.jsx'
 import studyApi from "./services/studyApi";
+import { Analytics } from '@vercel/analytics/react';
 
 window.studyApi = studyApi;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
